352ea5cc9438bd454e881963d52c78f0326b622f
Sam Norbury <sam.norbury@ukfast.co.uk>
2016-10-20 11:09:21 +0100

    Updating to local build on docs server to save time

--- deploy_docs.sh
+++ deploy_docs.sh
@@ -5,3 +5,9 @@ git pull --recurse-submodules origin master
 cd ukf && git pull origin master && cd -
 curl -XDELETE 'http://localhost:9200/documentation/'
 make populate-index
+sphinx-build -nW -b html -d build/doctrees source/ build/html
+make build/html/_static/css/app.css
+make build/html/_static/app.js
+rm -rf /var/www/vhosts/docs.ukfast.co.uk/htdocs
+cp -a build/html /var/www/vhosts/docs.ukfast.co.uk/htdocs
+chown -R nginx.nginx /var/www/vhosts/docs.ukfast.co.uk/htdocs
