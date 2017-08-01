9099f42f6db9ff1af6a5477d95624ee05182ba7b
Neil Lathwood <neil@lathwood.co.uk>
2017-08-01 11:44:42 +0100

    Updated test.sh to include length checks

--- tests.sh
+++ tests.sh
@@ -16,6 +16,14 @@ for f in $file_names; do
       echo "$f : Does not contain meta info"
       fail=1
     fi
+    title_size=$(grep '  :title:' $f | cut -d ':' -f3|wc -m)
+    if [[ "$title_size" -gt "66" ]]; then
+      echo "$f : Meta title is longer than 65 chars"
+    fi
+    descr_size=$(grep '  :description:' $f | cut -d ':' -f3|wc -m)
+    if [[ "$descr_size" -gt "166" ]]; then
+      echo "$f : Meta description is longer than 165 chars"
+    fi
   esac
 done
 
