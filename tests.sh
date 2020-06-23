30f94c333e563d99550445f3856e8e2a9c53d9da
darrentayloruk <30502984+darrentayloruk@users.noreply.github.com>
2020-06-23 15:00:49 +0100

    Updating populator to check for existing of the correct meta title (#624)
    Updating the test.sh to validate the right meta title
    Adding in clearer warning and Fail messages
    Updating tests to check for shorter meta as ? UKFast Documentation will be appended going forward to the template

--- tests.sh
+++ tests.sh
@@ -6,29 +6,43 @@ file_names=`(git diff --name-only $TRAVIS_COMMIT_RANGE || echo "") | tr '\n' ' '
 
 for f in $file_names; do
   if [ ! -f "$f" ]; then
-    echo "$f : Is not a file or does not exist anymore."
+    echo "$f : WARNING Is not a file or does not exist anymore."
     continue
   fi
   case $f in
     *.md )
-    meta=$(grep '\.\. meta::' $f >> /dev/null 2>&1; echo $?)
-    if [[ "$meta" != "0" ]]; then
-      echo "$f : Does not contain meta info"
+    
+    #check for new meta title
+    newtitle=$(grep '\.\. title:' $f >> /dev/null 2>&1; echo $?)
+    if [[ "$newtitle" != "0" ]]; then
+      echo "$f : FAIL Does not contain meta title new see readme"
       fail=1
     fi
-    title_size=$(grep '  :title:' $f | cut -d ':' -f3|wc -m)
+
+    if [[ "$f" =~ [A-Z] ]]; then
+       echo "$f : WARNING filepath is not lowercase"
+    fi
+
+    title_size=$(grep '\.\. title:' $f | cut -d ':' -f2 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' | wc -m)
     if [[ "$title_size" == "1" ]]; then
-      echo "$f : Meta title not specified"
+      echo "$f : WARNING Meta title not specified"
     fi
-    if [[ "$title_size" -gt "66" ]]; then
-      echo "$f : Meta title is longer than 65 chars"
+    #Meta title should exclude | UKFast Documentation
+    if [[ "$title_size" -gt "43" ]]; then
+      echo "$f : WARNING Meta title is $title_size - Max is 42 chars"
     fi
+
+    meta=$(grep '\.\. meta::' $f >> /dev/null 2>&1; echo $?)
+    if [[ "$meta" != "0" ]]; then
+      echo "$f : WARNING Does not contain meta info"
+    fi
+
     descr_size=$(grep '  :description:' $f | cut -d ':' -f3|wc -m)
     if [[ "$descr_size" == "1" ]]; then
-      echo "$f : Meta description not specified"
+      echo "$f : WARNING Meta description not specified"
     fi
     if [[ "$descr_size" -gt "166" ]]; then
-      echo "$f : Meta description is longer than 165 chars"
+      echo "$f : WARNING Meta description is longer than 165 chars"
     fi
   esac
 done
